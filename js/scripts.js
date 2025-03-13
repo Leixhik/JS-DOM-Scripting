20990b00e09662006436be4061d5fbe00dfb6c81
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-03-13 14:19:40 -0600

    Curso de JS con Juan Pablo de la Torre

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2'); // 0 o 1 elementos como máximo.
+// heading.textContent = 'Nuevo Heading';
+// heading.classList.add('nueva-clase');
+console.log(heading);
+
+
+// querySelectorAll
+
+
+
+// getElementById
