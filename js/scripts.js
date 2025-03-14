7eb27819e5116bcbc857c1d7d46a9efcd681159e
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-03-14 16:55:51 -0600

    aprendizaje básico de selectors

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,12 @@ console.log(heading);
 
 
 // querySelectorAll
-
-
+const enlaces = document.querySelectorAll('.navegacion a'); // 0 a todos los que concuerden con el selector.
+enlaces[0].textContent = 'Nuevo Texto para Enlace'; // cambio visible en el navegador.
+enlaces[0].classList.add('nueva-clase'); // Método para agregar nueva clase al nav.
+enlaces[0].classList.remove('navegacion__enlace'); // Método para remover clase del nav.
 
 // getElementById
+const heading2 = document.getElementById('heading'); //Como ya está buscando x ID no necesita el #.
+console.log(heading2);
+
