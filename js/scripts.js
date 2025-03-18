1ba052734d8a4e1230710fe3204a538f7163d421
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-03-18 15:53:40 -0600

    He aprendido acerca de como mostrar o capturar eventos en consola

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,14 @@ console.log(5);
 window.onscroll = function(){
     console.log('scrolling...');
 };
+
+// Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(evento){
+    console.log(evento);
+    evento.preventDefault();
+
+    // Util para validar un formulario
+    
+    console.log('Enviando Formulario');
+});
