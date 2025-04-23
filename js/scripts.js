7952438fda8e91d988529a4c6d836cf7d81c7040
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-23 15:52:56 -0600

    clase 149 terminada, aprendizaje acerca de obtención de contenido en un input, sus diferentes métodos mediante addEventListener

--- js/scripts.js
+++ js/scripts.js
@@ -35,25 +35,25 @@ console.log(nuevoEnlace);
 
 // Eventos
 
-console.log(1);
+// console.log(1);
 
-window.addEventListener('load', function(){ // Load espera a que el JS y los archivos que dependen del HTML estén listos.
-    console.log(2);
-});
+// window.addEventListener('load', function(){ // Load espera a que el JS y los archivos que dependen del HTML estén listos.
+//     console.log(2);
+// });
 
-window.onload = function(){ // Igual que el anterior.
-    console.log(3);
-};
+// window.onload = function(){ // Igual que el anterior.
+//     console.log(3);
+// };
 
-document.addEventListener('DOMContentLoaded', function(){ // Solo espera a que el HTML esté listo, no espera a CSS o imágenes.
-    console.log(4);
-});
+// document.addEventListener('DOMContentLoaded', function(){ // Solo espera a que el HTML esté listo, no espera a CSS o imágenes.
+//     console.log(4);
+// });
 
-console.log(5);
+// console.log(5);
 
-window.onscroll = function(){
-    console.log('scrolling...');
-};
+// window.onscroll = function(){
+//     console.log('scrolling...');
+// };
 
 // Seleccionar elementos y asociarles un evento
 const btnEnviar = document.querySelector('.boton--primario');
@@ -65,3 +65,29 @@ btnEnviar.addEventListener('click', function(evento){
     
     console.log('Enviando Formulario');
 });
+
+// Eventos de lo Inputs y Textareas
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+};
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e){
+    // console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value; // Guardar el valor en el objeto datos.
+
+    // console.log(e.target);
+
+    console.log(datos);
+}
